247b9370852f8bf71fc33a34c17822d4e32aea4c
Matthew Little <zone117x@gmail.com>
2019-07-23 10:38:33 -0400

    Disable SQLITE_THREADSAFE

--- libsqlite3-sys/build.rs
+++ libsqlite3-sys/build.rs
@@ -72,7 +72,7 @@ mod build_bundled {
             .flag("-DSQLITE_ENABLE_STAT4")
             .flag("-DSQLITE_HAVE_ISNAN")
             .flag("-DSQLITE_SOUNDEX")
-            .flag("-DSQLITE_THREADSAFE=1")
+            .flag("-DSQLITE_THREADSAFE=0")
             .flag("-DSQLITE_USE_URI")
             .flag("-DHAVE_USLEEP=1");
         if cfg!(feature = "unlock_notify") {
